954e844b49a80a9db55c576726183c54e5183334
Даниил Калинин <crm.v.28556@gmail.com>
2026-04-18 13:23:04 +0300

    Add Parser Console app and base structure

---
+++ ParserAgent/Parsers/CoinMarketAppParser.cs
@@ -0,0 +1,13 @@
+﻿using ParserAgent.Parsers.Interfaces;
+using Microsoft.Playwright;
+
+namespace ParserAgent.Parsers
+{
+    internal class CoinMarketAppParser : IParser
+    {
+        public async Task Parse(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ParserAgent/Program.cs
@@ -0,0 +1,5 @@
+﻿using ParserAgent.Parsers;
+
+var parser = new CoinMarketAppParser();
+
+await parser.Parse("https://coinmarketapp.com/coins");
